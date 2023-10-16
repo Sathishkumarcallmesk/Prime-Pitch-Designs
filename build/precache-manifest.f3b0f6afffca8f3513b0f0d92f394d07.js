d3b645cc877a56a65bc051a42df8e1a711e89974
Sathishkumarcallmesk <sathishkumarcallmesk@gmail.com>
2023-10-16 20:56:23 +0530

    build

---
+++ build/precache-manifest.f3b0f6afffca8f3513b0f0d92f394d07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3a5a2c7d46bbfcb2768e5893f23e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96cf4a93192214bac6b4",
+    "url": "/static/css/2.aadebbfd.chunk.css"
+  },
+  {
+    "revision": "08ee61ee96c6fe7177bc",
+    "url": "/static/css/main.59579de2.chunk.css"
+  },
+  {
+    "revision": "96cf4a93192214bac6b4",
+    "url": "/static/js/2.15bdf577.chunk.js"
+  },
+  {
+    "revision": "08ee61ee96c6fe7177bc",
+    "url": "/static/js/main.1d993dff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
